a886695a1b7de23cde41a3fb47b1fc81b48a7891
LuciaEckl <lucia_eckl@yahoo.de>
2018-06-08 05:20:08 -0400

    Mit auskommentierter Print-Ausgabe

--- analyze.py
+++ analyze.py
@@ -13,7 +13,7 @@ import numpy as np
 
 import wiimote
 
-import wiimote_node as wiinode
+import wiimote_node
 
 if __name__ == '__main__':
     import sys
@@ -38,12 +38,34 @@ if __name__ == '__main__':
 
     pw1Node = fc.createNode('PlotWidget', pos=(0, -150))
     pw1Node.setPlot(pw1)
+    #print(pw1Node.update_all_sensors())
+
+    pw2 = pg.PlotWidget()
+    layout.addWidget(pw2, 0, 1)
+    pw2.setYRange(0, 1024)
+
+    pw2Node = fc.createNode('PlotWidget', pos=(0, 100))
+    pw2Node.setPlot(pw2)
+
+    pw3 = pg.PlotWidget()
+    layout.addWidget(pw3, 0, 1)
+    pw3.setYRange(0, 1024)
+
+    pw3Node = fc.createNode('PlotWidget', pos=(0, 150))
+    pw3Node.setPlot(pw3)
+
 
     wiimoteNode = fc.createNode('Wiimote', pos=(0, 0), )
-    bufferNode = fc.createNode('Buffer', pos=(150, 0))
+    buffer1Node = fc.createNode('Buffer', pos=(150, 0))
+    buffer2Node = fc.createNode('Buffer', pos=(100, 50))
+    buffer3Node = fc.createNode('Buffer', pos=(200, 50))
 
-    fc.connectTerminals(wiimoteNode['accelX'], bufferNode['dataIn'])
-    fc.connectTerminals(bufferNode['dataOut'], pw1Node['In'])
+    fc.connectTerminals(wiimoteNode['accelX'], buffer1Node['dataIn'])
+    fc.connectTerminals(wiimoteNode['accelY'], buffer2Node['dataIn'])
+    fc.connectTerminals(wiimoteNode['accelZ'], buffer3Node['dataIn'])
+    fc.connectTerminals(buffer1Node['dataOut'], pw1Node['In'])
+    fc.connectTerminals(buffer2Node['dataOut'], pw2Node['In'])
+    fc.connectTerminals(buffer3Node['dataOut'], pw3Node['In'])
 
     win.show()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
