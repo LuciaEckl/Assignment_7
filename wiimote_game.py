20cae939415b440c404821540f6355d47ae25408
Katharina Lichtner <Katharina.Lichtner@stud.uni-regensburg.de>
2018-06-12 09:50:15 -0400

    Game fertig

--- wiimote_game.py
+++ wiimote_game.py
@@ -15,16 +15,12 @@ instructions.
 
 sys.stdout.write("Press the 'sync' button on the back of your Wiimote Plus \n" +
                  "Press <return> once the Wiimote's LEDs start blinking. \n")
-sys.stdout.write("Press the displayed buttons to get points for your highscore \n")
-
-
 
 gameOn = True
 firstTask = False
 secondTask = False
 thirdTask = False
-highscore = 0
-sum = 0
+
 
 if len(sys.argv) == 1:
     addr, name = wiimote.find()[0]
@@ -51,40 +47,23 @@ for i in range(5):
         time.sleep(0.05)
 
 
-
-#def checkState(button):
-  #  print("hey")
- #   if button == "A" and not firstTask:
-  #      firstTask = True
-
-
-
 # By Katharina Lichtner
-# When Button A and Button B are pressed at the same time the remote
-# rumbles and a webpage is opened.
+# The user should press the button which is displayed on the command line.
+# after every task the next led on the wiimote starts to light
+# at the last task the wiimote rumbles and a webpage is opened.
 # source: https://stackoverflow.com/questions/4302027/how-to-open-a-url-in-python last access: 06.06.2018
 while gameOn:
+    sys.stdout.write("Press the displayed buttons to get points for your highscore \n")
     sys.stdout.write("\n Press the A Button \n")
     print("Statusfirsttast", firstTask)
     while True:
         homepage_link = "http://www.kamelrechner.eu/de"
-        #status_code = 0
         if (wm.buttons["A"] is True) and (firstTask is False):
-          #  checkState("A")
-
             wm.leds[1] = True
             sum += 10
             firstTask = True
             print(firstTask, wm.buttons)
             sys.stdout.write("Press the B Button \n")
-            #False
-           #checkState()
-           # webbrowser.open(homepage_link)
-           # wm.rumble(0.1)
-       # elif wm.buttons["A"] is False:
-            #wm.rumble(0.1)
-        elif (wm.buttons["A"] is False) and (firstTask is False):
-            sys.stdout.write("Try it again. Press the A Button \n")
 
         if ((wm.buttons["B"] is True) and (firstTask is True) and (secondTask is False)):
             sys.stdout.write("Press the A and B Button at the same time \n")
@@ -92,30 +71,16 @@ while gameOn:
             wm.leds[2] = True
             sum += 10
             secondTask = True
-        else:
-            sys.stdout.write("Try it again. Press the B Button \n")
-            #sys.stdout.write("Press the A Button and the B Button at the same time\n")
-        if ((wm.buttons["A"] is True) and (wm.buttons["B"] is True) and (firstTask is True) and (secondTask is True) and (thirdTask is False)):
-          #  sys.stdout.write("Press the B Button \n"))
-            #sys.stdout.write("Press the A Button and the B Button at the same time\n")
+        if ((wm.buttons["A"] is True) and (wm.buttons["B"] is True) and (firstTask is True)
+                and (secondTask is True) and (thirdTask is False)):
             wm.leds[3] = True
             sum += 10
             thirdTask = True
-        else:
-            sys.stdout.write("Try it again. Press the A Button and the B Button at the same time \n")
-       # else:
-        #    sum -= 10
-        #    wm.rumble(0.1)
-        #    print("highsocre", sum)
-        if firstTask is True and secondTask is True and thirdTask is True:
-           # wm.rumble(0.1)
+        if firstTask is True and secondTask is True and thirdTask is True and gameOn is True:
+            wm.rumble(0.5)
             sys.stdout.write("You won! Now a website will open! \n")
             webbrowser.open(homepage_link)
             gameOn = False
         else:
-            #wm.leds[1] = False
-          #  sys.stdout.write("You pressed the wrong button \n")
             pass
         time.sleep(0.05)
-
-
